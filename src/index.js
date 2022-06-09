4b34008bfc9c8d5e2b6a08cc3eb29e2376b83e57
AdilNehal22 <adilnehal80@gmail.com>
2022-06-09 08:30:56 +0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import './index.css'
+import data from './data.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Books = data;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function Greeting() {
+  return (
+    //you are passing the params as the func Book takes props
+    //for this pass as a 'children' in prop pops-children
+    <section className="bookLists">
+      {Books.map((obj) => {
+        return <Book Imag={obj.Img} titleBook={obj.titleBook} Author={obj.Author} />
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  console.log('props>>>>>>', props);
+  const { Imag, titleBook, Author } = props;
+  return (
+    <article className="book">
+      <img src='https://encrypted-tbn1.gstatic.com/imagesq=tbn:ANd9GcRlGbd-cBXub95mkibUSCRoH8v6hw9VvGXVM8ads0R0kHccRs1t' />
+      <img src=''/>
+      <h1>{titleBook}</h1>
+      <p>{Author}</p>
+    </article>
+  );
+}
+
+
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
